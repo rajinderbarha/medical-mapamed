e8a6e22afeef7c535554f1fd58454f3316634519
rajinderbarha <rajinderbarha@gmail.com>
2025-06-20 10:49:33 +0530

    fix border and shadow of popular card

--- src/common/PopularCard.tsx
+++ src/common/PopularCard.tsx
@@ -8,7 +8,7 @@ interface PopularCardProps {
 const PopularCard: React.FC<PopularCardProps> = ({ title, icon = 'jam:medical' }) => {
     return (
         <>
-            <button className="cursor-pointer bg-white border border-base-100 shadow-1 py-4 px-5 rounded-[8px] transition-all duration-300 hover:shadow-1 hover:border-2  active:border-accent  active:border-2">
+            <button className="cursor-pointer bg-white border border-base-100 shadow-1 py-4 px-5 rounded-[8px] transition-all duration-300 hover:shadow-2 hover:border-2 active:shadow-2  active:border-accent  active:border-1 ">
                 <div className="size-[43px] flex items-center justify-center bg-primary-50 rounded-full mb-2">
                     <Icon icon={icon} className='size-5 text-primary-darker' />
                 </div>
